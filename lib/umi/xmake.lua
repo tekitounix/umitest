15c08b22c75b3dce61122099489cdf4899a30a18
tekitounix <sicksteen@me.com>
2026-02-05 07:02:13 +0900

    build: update build system configuration
    - Refactor root xmake.lua
    - Update lib/umi/xmake.lua module configuration

--- lib/umi/xmake.lua
+++ lib/umi/xmake.lua
@@ -7,6 +7,61 @@
 --       add_deps("umi")  -- or specific: "umi.core", "umi.dsp", "umi.midi"
 -- =====================================================================
 
+-- =====================================================================
+-- Project Configuration (library-side)
+-- =====================================================================
+
+if os.isdir(path.join(os.projectdir(), ".refs/arm-embedded-xmake-repo")) then
+    add_repositories("arm-embedded " .. path.join(os.projectdir(), ".refs/arm-embedded-xmake-repo"))
+else
+    add_repositories("arm-embedded https://github.com/tekitounix/arm-embedded-xmake-repo.git")
+end
+
+add_requires("arm-embedded", {optional = true})
+
+set_languages("c++23")
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate", {outputdir = ".", lsp = "clangd"})
+set_warnings("all", "extra", "error")
+
+option("board")
+    set_default("stm32f4")
+    set_showmenu(true)
+    set_description("Target board")
+    set_values("stm32f4", "stub")
+option_end()
+
+option("kernel")
+    set_default("mono")
+    set_showmenu(true)
+    set_description("Kernel configuration")
+    set_values("mono", "micro")
+option_end()
+
+option("coverage")
+    set_default(false)
+    set_showmenu(true)
+    set_description("Enable code coverage instrumentation")
+option_end()
+
+option("build_type")
+    set_default("dev")
+    set_showmenu(true)
+    set_description("Build type for kernel/app (dev=skip signature, release=require signature)")
+    set_values("dev", "release")
+option_end()
+
+if has_config("coverage") and is_mode("debug") then
+    add_cxflags("--coverage", {force = true})
+    add_ldflags("--coverage", {force = true})
+end
+
+if is_config("build_type", "release") then
+    add_defines("UMIOS_BUILD_TYPE=umi::kernel::BuildType::RELEASE", {public = true})
+else
+    add_defines("UMIOS_BUILD_TYPE=umi::kernel::BuildType::DEVELOPMENT", {public = true})
+end
+
 -- Get the umi lib directory (directory containing this script)
 local umi_dir = os.scriptdir()
 local lib_dir = path.directory(umi_dir)
@@ -19,6 +74,14 @@ includes(path.join(umi_dir, "mmio"))
 
 -- Include umi.test (testing framework)
 includes(path.join(lib_dir, "umi/test"))
+
+-- Include library subprojects
+includes(path.join(umi_dir, "bench"))
+includes(path.join(umi_dir, "ref"))
+includes(path.join(umi_dir, "fs"))
+includes(path.join(umi_dir, "fs/test"))
+includes(path.join(umi_dir, "usb/test"))
+includes(path.join(umi_dir, "port/test"))
 
 -- =====================================================================
 -- UMI Shell Library (platform-independent shell primitives)
@@ -44,7 +107,6 @@ target("umi.core")
     add_includedirs(path.join(lib_dir, "umi/kernel"), {public = true})
     add_includedirs(path.join(lib_dir, "umi/adapter"), {public = true})
     add_includedirs(path.join(lib_dir, "umi"), {public = true})
-    -- For backward compatibility with umios/* includes (via symlink lib/umios -> lib/umi)
     add_includedirs(lib_dir, {public = true})
 target_end()
 
